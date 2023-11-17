0280236a5b6fd067d7b669e4eae7ec512944da2f
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-11-17 03:10:25 +0100

    Create Arguments to Binary addition.ts

---
+++ Codewar/8 Kyu/Arguments to Binary addition.ts
@@ -0,0 +1,20 @@
+// Given an array add all the number elements and return the binary equivalent of that sum;
+// to make the code bullet proof anything else than a number should be addeded as 0 since it can't be addeded.
+
+function arr2bin(array: []) {
+  if (!Array.isArray(array)) {
+    throw new Error('Input must be an array');
+  }
+
+  let sum = 0;
+
+  // Iterate through each element in the array
+  for (const element of array) {
+    // If the element is a number, add it to the sum
+    // If it's not a number, treat it as 0
+    sum += typeof element === 'number' ? element : 0;
+  }
+
+  // Convert the sum to binary and return it
+  return sum.toString(2);
+}
