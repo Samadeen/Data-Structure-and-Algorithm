6427fec1515a15172f2fc52b46ba31ab1b946dc9
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-11-18 21:52:51 +0100

    Create Vowel Count.ts

---
+++ Codewar/7 Kyu/Vowel Count.ts
@@ -0,0 +1,25 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+function getCount(str: string) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === 'a' ||
+      str[i] === 'e' ||
+      str[i] === 'i' ||
+      str[i] === 'o' ||
+      str[i] === 'u'
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// function getCount(str) {
+//   return (str.match(/[aeiou]/gi) || []).length;
+// }
