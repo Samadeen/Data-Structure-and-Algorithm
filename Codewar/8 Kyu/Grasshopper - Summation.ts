20499b48ea8853e2143dfb6a752bd5f7ab712666
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-11-24 21:44:10 +0100

    Create Grasshopper - Summation.ts

---
+++ Codewar/8 Kyu/Grasshopper - Summation.ts
@@ -0,0 +1,13 @@
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+var summation = function (num: number) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+const summation1 = (num: number) => {
+  return (num * (num + 1)) / 2;
+};
