13a39cbb3fe263b980146ebdb55239bbec4a0209
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-12-17 18:12:01 +0100

    Area of a Square.ts

---
+++ Codewar/8 Kyu/Area of a Square.ts
@@ -0,0 +1,4 @@
+/* Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.*/
+function squareArea(A: number) {
+  return Number(Math.pow((2 * A) / 3.1416, 2).toFixed(2));
+}
